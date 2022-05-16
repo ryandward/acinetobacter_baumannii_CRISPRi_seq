5af4734588b54fca30d968d4537755f55d8df937
Ryan Ward <ryandward@gmail.com>
2022-05-16 18:11:33 -0500

    added fosfomycin stuff to scratch from jennifer's data

--- scratch.R
+++ scratch.R
@@ -1,111 +1,52 @@
-p_load(tidyverse, ComplexHeatmap, colorspace, viridis)
-
-conditions.full <- tibble(condition = c(
-	"None_0_T1 - None_0_T0",
-	"None_0_T2 - None_0_T0",
-	"Colistin_0.44_T1 - None_0_T0",
-	"Colistin_0.44_T2 - None_0_T0",
-	"Rifampicin_0.34_T1 - None_0_T0",
-	"Rifampicin_0.34_T2 - None_0_T0",
-	"Imipenem_0.06_T1 - None_0_T0",
-	"Imipenem_0.06_T2 - None_0_T0",
-	"Imipenem_0.09_T1 - None_0_T0",
-	"Meropenem_0.11_T1 - None_0_T0",
-	"Meropenem_0.11_T2 - None_0_T0",
-	"Meropenem_0.17_T1 - None_0_T0",
-	"Meropenem_0.17_T2 - None_0_T0"))
-
-conditions.interest <- fread("interest.tsv", sep = "\t")
-
-conditions.all <- rbind(conditions.full, conditions.interest) %>% unique
-
-median_melted_results <- fread("Results/median_melted_results.tsv.gz")
-
-
-depleted.long <- 
-	conditions.all %>% 
-	inner_join(
-		median_melted_results %>%
-			filter(condition %in% c(
-				"None_0_T2 - None_0_T0",
-				"Meropenem_0.17_T2 - None_0_T2",
-				"Imipenem_0.09_T2 - None_0_T2") &
-					type == "perfect")) 
-
-mixed <- depleted.long %>%
+mixed <- fread("../acinetobacter_baumannii_chemical_genomics/fosf.tsv") %>% 
+	as_tibble %>% 
+	dplyr::select(AB19606, medLFC, FDR, condition, type) %>% 
+	rbind(median_melted_results[condition %like% "Imipenem_0.09_T2 - None_0_T2"] %>% 
+					dplyr::select(AB19606, medLFC, FDR, condition, type)) %>%	
+	filter(type == "perfect") %>%
 	mutate(
-		depleted = (medLFC < 0 & FDR < 0.01), 
-		resistant = (medLFC > 0 & FDR < 0.01)) %>% 
-	select(unique_name, condition, depleted, resistant) %>% 
-	pivot_longer(!c(condition, unique_name)) %>% 
-	mutate(condition = gsub(" - None_0_T([1-9])", "-T\\1", condition)) %>%
-	mutate(condition = gsub(" - None_0_T0", "-T0", condition)) %>% 
-	mutate(condition = gsub("_", " ", condition)) %>% 
-	mutate(condition = gsub("None 0", "No drug", condition)) %>%
+		depleted = (medLFC < 1 & FDR < 0.01), 
+		resistant = (medLFC > 1 & FDR < 0.01)) %>% 
+	select(AB19606, condition, depleted, resistant) %>% 
+	pivot_longer(!c(condition, AB19606)) %>% 
+	mutate(condition = gsub("_[0-9\\.]+_", " ", condition)) %>% 
 	unite("condition", c("condition", "name"), sep = " ") %>% 
-	pivot_wider(id_cols = unique_name, names_from = condition, values_from = value)
+	pivot_wider(id_cols = AB19606, names_from = condition, values_from = value) 
 
-## distinct mode
-mixed.matrix.distinct <- mixed %>%
+mixed.matrix <- mixed %>%
 	make_comb_mat(mode = "distinct")
 
 p <- UpSet(
-	mixed.matrix.distinct, 
+	mixed.matrix, 
 	top_annotation = HeatmapAnnotation(
-		"Distinct Sets" = anno_barplot(
-			comb_size(mixed.matrix.distinct),
+		"Intersection" = anno_barplot(
+			comb_size(mixed.matrix),
 			border = FALSE, 
 			height = unit(8, "cm"),
-			add_numbers = T), 
-annotation_name_gp = gpar(fontsize = 10),		
-show_annotation_name = TRUE),
+			add_numbers = T,
+			gp = gpar(fill = c("grey", rep("red", 1), rep("grey", 6)), lty = "blank")), 
+		show_annotation_name = FALSE),
 	right_annotation = rowAnnotation(
-		"Genes Significant Beyond Induction" = anno_barplot(
-			set_size(mixed.matrix.distinct),
+		"Genes Significant Beyond Induction at T2" = anno_barplot(
+			set_size(mixed.matrix),
 			border = FALSE,
 			gp = gpar(
-				fill = viridis(5, direction = -1, alpha = 0.35, option = "plasma") %>% rep(each = 2), 
+				# fill = viridis(set_size(mixed.matrix)/2 %>% length, direction = -1, alpha = 0.5),
+				# fill = c(rep("light blue", 2), rep("coral", 2)),
+				fill = viridis(2, direction = -1, alpha = 0.35) %>% rep(each = 2), 
 				col = c("red", "black", "red", "black"),
 				lwd = 3,
 				lty = c("dotted", "solid", "dotted", "solid")),
 			width = unit(6, "cm"),
 			add_numbers = T),
 		annotation_name_gp = gpar(fontsize = 8)),
-	set_order = set_name(mixed.matrix.distinct),
+	set_order = set_name(mixed.matrix),
 	row_names_gp = grid::gpar(fontsize = 10),
-	comb_col = darken(viridis(5, direction = -1)[comb_degree(mixed.matrix.distinct)], 0.35))
-
-print(p)
-
-
-## intersect mode
-mixed.matrix.intersect <- mixed %>%
-	make_comb_mat(mode = "intersect")
-
-p <- UpSet(
-	mixed.matrix.intersect, 
-	top_annotation = HeatmapAnnotation(
-		"Intersect Sets" = anno_barplot(
-			comb_size(mixed.matrix.intersect),
-			border = FALSE, 
-			height = unit(8, "cm"),
-			add_numbers = T), 
-		annotation_name_gp = gpar(fontsize = 10),		
-		show_annotation_name = TRUE),
-	right_annotation = rowAnnotation(
-		"Genes Significant Beyond Induction" = anno_barplot(
-			set_size(mixed.matrix.intersect),
-			border = FALSE,
-			gp = gpar(
-				fill = viridis(5, direction = -1, alpha = 0.35, option = "plasma") %>% rep(each = 2), 
-				col = c("red", "black", "red", "black"),
-				lwd = 3,
-				lty = c("dotted", "solid", "dotted", "solid")),
-			width = unit(6, "cm"),
-			add_numbers = T),
-		annotation_name_gp = gpar(fontsize = 8)),
-	set_order = set_name(mixed.matrix.intersect),
-	row_names_gp = grid::gpar(fontsize = 10),
-	comb_col = darken(viridis(5, direction = -1)[comb_degree(mixed.matrix.intersect)], 0.35))
+	comb_col = c("black", rep("red", 1), rep("black", 6)),
+	row_labels = c(
+		as.expression(bquote(Fosfomycin[" Vulnerable "])), 
+		as.expression(bquote(Fosfomycin[" Resistant "])),
+		as.expression(bquote(Imipenem[" Vulnerable "])),
+		as.expression(bquote(Imipenem[" Resistant "]))))
 
 print(p)
