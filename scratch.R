c6490949c59db93bcbc461064c3d9c722041ec62
Ryan Ward <ryandward@gmail.com>
2022-06-07 17:55:09 -0500

    updates for figures

--- scratch.R
+++ scratch.R
@@ -1,52 +1,16 @@
-mixed <- fread("fosf.tsv") %>% 
-	as_tibble %>% 
-	dplyr::select(AB19606, medLFC, FDR, condition, type) %>% 
-	rbind(median_melted_results[condition %like% "Imipenem_0.09_T2 - None_0_T2"] %>% 
-					dplyr::select(AB19606, medLFC, FDR, condition, type)) %>%	
-	filter(type == "perfect") %>%
-	mutate(
-		depleted = (medLFC < 1 & FDR < 0.01), 
-		resistant = (medLFC > 1 & FDR < 0.01)) %>% 
-	select(AB19606, condition, depleted, resistant) %>% 
-	pivot_longer(!c(condition, AB19606)) %>% 
-	mutate(condition = gsub("_[0-9\\.]+_", " ", condition)) %>% 
-	unite("condition", c("condition", "name"), sep = " ") %>% 
-	pivot_wider(id_cols = AB19606, names_from = condition, values_from = value) 
+median_melted_results %>% filter(medLFC < -1 & FDR < 0.05) %>% filter(condition %in% c("None_0_T1 - None_0_T0")) %>% filter(type == "perfect") -> T1_stuff
 
-mixed.matrix <- mixed %>%
-	make_comb_mat(mode = "distinct")
+median_melted_results %>% filter(medLFC < -1 & FDR < 0.05) %>% filter(condition %in% c("None_0_T1 - None_0_T0") & unique_name %in% T1_stuff$unique_name) %>% filter(type == "perfect") %>% pull(medLFC) %>% density -> T1_shape
 
-p <- UpSet(
-	mixed.matrix, 
-	top_annotation = HeatmapAnnotation(
-		"Intersection" = anno_barplot(
-			comb_size(mixed.matrix),
-			border = FALSE, 
-			height = unit(8, "cm"),
-			add_numbers = T,
-			gp = gpar(fill = c("grey", rep("red", 1), rep("grey", 6)), lty = "blank")), 
-		show_annotation_name = FALSE),
-	right_annotation = rowAnnotation(
-		"Genes Significant Beyond Induction at T2" = anno_barplot(
-			set_size(mixed.matrix),
-			border = FALSE,
-			gp = gpar(
-				# fill = viridis(set_size(mixed.matrix)/2 %>% length, direction = -1, alpha = 0.5),
-				# fill = c(rep("light blue", 2), rep("coral", 2)),
-				fill = viridis(2, direction = -1, alpha = 0.35) %>% rep(each = 2), 
-				col = c("red", "black", "red", "black"),
-				lwd = 3,
-				lty = c("dotted", "solid", "dotted", "solid")),
-			width = unit(6, "cm"),
-			add_numbers = T),
-		annotation_name_gp = gpar(fontsize = 8)),
-	set_order = set_name(mixed.matrix),
-	row_names_gp = grid::gpar(fontsize = 10),
-	comb_col = c("black", rep("red", 1), rep("black", 6)),
-	row_labels = c(
-		as.expression(bquote(Fosfomycin[" Vulnerable "])), 
-		as.expression(bquote(Fosfomycin[" Resistant "])),
-		as.expression(bquote(Imipenem[" Vulnerable "])),
-		as.expression(bquote(Imipenem[" Resistant "]))))
+median_melted_results %>% filter(medLFC < -1 & FDR < 0.05) %>% filter(condition %in% c("None_0_T2 - None_0_T0") & unique_name %in% T1_stuff$unique_name) %>% filter(type == "perfect") %>% pull(medLFC) %>% density -> T2_shape
 
-print(p)
+plot(T2_shape, col = "red", ylim = c(0, 1), xlim = c(-15, 0))
+
+lines(T1_shape, col = "blue")
+
+
+###########
+
+# genes with phenotypes
+median_melted_results %>% filter(condition %in% (interest %>% mutate(condition = gsub("- None_0_T1", "- None_0_T0", condition)) %>% mutate(condition = gsub("- None_0_T2", "- None_0_T0", condition)) %>% pull("condition"))) %>% filter(FDR < 0.05) %>% filter(abs(medLFC) > 1) %>% filter(type != "perfect") %>% select(unique_name) %>% unique
+
