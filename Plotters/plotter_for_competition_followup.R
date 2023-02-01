1ef0f9a471929d6f735835ffe8cd37493cf8bca6
Ryan Ward <ryandward@gmail.com>
2023-02-01 13:40:21 -0600

    fix: recovered lost code to generate intermediate table

--- Plotters/plotter_for_competition_followup.R
+++ Plotters/plotter_for_competition_followup.R
@@ -3,15 +3,17 @@ library(conflicted)
 library(pacman)
 
 # Use pacman to load the following packages
-p_load(data.table, tidyverse, broom, modelr)
+p_load(data.table, tidyverse, broom, modelr, Hmisc)
 p_load_current_gh("DoseResponse/drcData", "ryandward/drc", "hrbrmstr/hrbrthemes")
+
+# Conflicts
+conflict_prefer("select", "dplyr")
+conflict_prefer("filter", "dplyr")
+conflict_prefer("mutate", "dplyr")
+conflict_prefer("summarize", "dplyr")
 
 # Define a custom theme for the ggplot visualization
 doc_theme <- theme_ipsum(base_family = "Arial", caption_margin = 12, axis_title_size = 12, axis_col = "black")
-
-# Prefer the dplyr version of select and filter functions in case of conflicts
-conflict_prefer("select", "dplyr")
-conflict_prefer("filter", "dplyr")
 
 # Read the data from "competition_followups.tsv"
 competition_followups <- fread("competition_followups.tsv")
@@ -21,6 +23,27 @@ drug_sets <- competition_followups %>%
   select(set, antibiotic) %>% 
   filter(antibiotic != "none") %>% 
   unique
+
+# Create additional binary variable to indicate whether a strain has been treated with any drug, 
+# turn empty named strains into "control", then dcast into wide-form data
+competition_raw <- competition_followups %>% 
+	mutate(
+		treatment = case_when(antibiotic == "none" ~ "none", TRUE ~ "drug"),
+		treatment = factor(treatment, levels = c("none", "drug")),
+		`unique name` = case_when(`unique name` == "" ~ "control", TRUE ~ `unique name`)) %>%
+	dcast(set + strains + `unique name` ~ timing + treatment + replicate, value.var = "proportion")
+
+# Define list of data-filled columns, which in our case begin with T for T0/T1/T2
+data_columns <- competition_raw %>% 
+	colnames %>% `[`(competition_raw %>% colnames %>% grepl("^T", .))
+
+# Beautify data a little more 
+competition_raw <- competition_raw %>% 
+	select(set, strains, `unique name`, all_of(data_columns)) %>% 
+	melt(id.vars = c("set", "strains", "unique name")) %>% 
+	mutate(variable = gsub("_d$", "", variable)) %>%
+	separate(variable, c("timing", "treatment", "replicate")) %>%
+	mutate(treatment = factor(treatment, levels = c("none", "drug"))) 
 
 # Filter the raw competition data for "timing == "T0"", add a column "treatment" with the value "drug", 
 # bind the filtered data to the original competition data with fill, join with the drug_sets, 
