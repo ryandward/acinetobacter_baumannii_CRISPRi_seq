23144e21e9ed15461d96d13e7afa03b1a302c649
Ryan Valverde de Ward <ryandward@gmail.com>
2023-02-02 23:29:53 -0600

    change: tSNE now plots counts and not change

--- tsne.R
+++ tsne.R
@@ -1,71 +1,49 @@
 # Load data into melted_results
-melted_results <- fread("Results/melted_results.tsv.gz")
+aba_all <- fread(
+  "all_counts_seal.tsv.gz",
+  header = FALSE,
+  col.names = c("spacer", "count", "condition"))
+
+curated_names <- fread("curated_names.tsv")
+
+aba_all <-
+	aba_all %>% 
+	group_by(condition) %>% 
+	mutate(cpm = 10^6 * count / sum(count)) %>% 
+	left_join(aba_key) %>% 
+	left_join(
+		curated_names %>% 
+		rename("locus_tag" = "AB19606")) %>% 
+	left_join(melted_results %>% select(unique_name, Pathway) %>% unique)
 
 # Define the conditions to be compared to T0
-tSNE_conditions <- c(
-	"None_0_T1 - None_0_T0",
-	"None_0_T2 - None_0_T0",
-	"Colistin_0.44_T1 - None_0_T0",
-	"Colistin_0.44_T2 - None_0_T0",
-	"Rifampicin_0.34_T1 - None_0_T0",
-	"Rifampicin_0.34_T2 - None_0_T0",
-	"Imipenem_0.06_T1 - None_0_T0",
-	"Imipenem_0.06_T2 - None_0_T0",
-	"Imipenem_0.09_T1 - None_0_T0",
-	"Imipenem_0.09_T2 - None_0_T0",
-	"Meropenem_0.11_T1 - None_0_T0",
-	"Meropenem_0.11_T2 - None_0_T0",
-	"Meropenem_0.17_T1 - None_0_T0",
-	"Meropenem_0.17_T2 - None_0_T0")
-
-# Extract timing from conditions
-tSNE_timing <- tSNE_conditions %>% stringr::str_extract("T[12]")
-
-# Create metadata data.table
-tSNE_meta <- data.table(condition = tSNE_conditions, timing = tSNE_timing)
-
-# Join metadata with melted results
-tSNE_results <- tSNE_meta %>% inner_join(melted_results)
-
-# Get rid of timing information from the "shift" column
-tSNE_results <- tSNE_results %>% mutate(shift = gsub("_T[12]", "", shift))
-
-# Spread data for tSNE input
-tSNE_results_spread <- tSNE_results %>% 
-	data.table::dcast(timing + spacer + type + Pathway + unique_name ~ shift, value.var = "LFC")
+aba_all_spread <- aba_all %>% 
+	ungroup() %>% 
+	filter(condition %in% (aba_design %>% filter(experiment == "tube") %>% pull(condition))) %>%
+	data.table() %>%
+	dcast(Pathway + spacer + type + unique_name ~ condition, value.var = "count", fill = 0)
 
 # Load Rtsne library
 library(Rtsne)
 
 # Perform t-SNE
-tsne_out <- tSNE_results_spread %>%
-	select(-timing, -spacer, -type, -Pathway, -unique_name) %>%
-	data.matrix %>%
-	Rtsne(perplexity = 200)
+tsne_out <- aba_all_spread %>%
+  select(-Pathway, -spacer, -type, -unique_name) %>%
+  data.matrix %>%
+  Rtsne()
 
 # Combine t-SNE results with metadata
-tsne_dt <- tSNE_results_spread %>%
-	select(timing, spacer, type, Pathway, unique_name) %>%
-	cbind(tsne_out$Y %>% as_tibble)
-
-# Define a function for color scaling
-scm = function(palette = cols) {
-	scale_color_manual(values = palette, na.value = alpha("light gray", 0.25))
-}
+tsne_dt <- aba_all_spread %>%
+  select(Pathway, spacer, type, unique_name) %>%
+  cbind(tsne_out$Y %>% as_tibble)
 
 # Join data frames and create a new column "Pathway"
-joined_dt <- inner_join(tsne_dt, aba_key) %>%
-	mutate(
-		Pathway = case_when(
-			unique_name %like% "atp" ~ "ATP",
-			unique_name %like% "cyo" ~ "CYO",
-			unique_name %like% "nuo" ~ "NADH"))
-
 
 # Plot the data
-ggplot(joined_dt, aes(x = V1, y = V2)) + 
-	geom_point(aes(colour = Pathway), alpha = 0.5) +
-	scale_color_manual(values = c("red", "blue", "green")) +
-	doc_theme +
-	facet_grid(timing ~ type)
+tsne_dt %>% 
+	filter(Pathway != "Other" | is.na(Pathway))  %>% 
+	ggplot(aes(x = V1, y = V2)) + 
+  geom_point(aes(colour = Pathway), alpha = 0.5) +
+  doc_theme +
+  facet_grid( ~ type)
 
