b603b908c21bdfc3c044f93ad4a7e4e059bad9dc
Ryan Ward <ryandward@gmail.com>
2023-04-11 23:28:41 -0500

    optimize: code for multiple hypothesis testing

--- drc_logistic_functions.R
+++ drc_logistic_functions.R
@@ -1,34 +1,35 @@
-BC.5.logistic <- function (fixed = c(NA, NA, NA, NA, NA), names = c("b", "c", "d", "e", "f"), ...) 
-{
-	numParm <- 5
-	if (!is.character(names) | !(length(names) == numParm)) {
-		stop("Not correct 'names' argument")
-	}
-	if (!(length(fixed) == numParm)) {
-		stop("Not correct length of 'fixed' argument")
-	}
-	return(braincousens_logistic(names = names, fixed = fixed, fctName = as.character(match.call()[[1]]), ...))
-}
+# Brain-Cousens Hormetic DRC Logistic Model (Not LLogistic)
+# Ryan Ward
+# Tuesday, April 11, 2023
 
-L.4.mod <- function (fixed = c(NA, NA, NA, NA), names = c("b", "c", "d", "e"), ...) 
-{
-	numParm <- 4
-	if (!is.character(names) | !(length(names) == numParm)) {
-		stop("Not correct 'names' argument")
+BC.5.logistic <-
+	function (fixed = c(NA, NA, NA, NA, NA),
+						names = c("b", "c", "d", "e", "f"),
+						...)
+	{
+		numParm <- 5
+		if (!is.character(names) | !(length(names) == numParm)) {
+			stop("Not correct 'names' argument")
+		}
+		if (!(length(fixed) == numParm)) {
+			stop("Not correct length of 'fixed' argument")
+		}
+		return(braincousens_logistic(
+			names = names,
+			fixed = fixed,
+			fctName = as.character(match.call()[[1]]),
+			...
+		))
 	}
-	if (!(length(fixed) == numParm)) {
-		stop("Not correct length of 'fixed' argument")
-	}
-	return(mod_logistic(fixed = c(fixed, 1), names = c(names, "f"), fctName = as.character(match.call()[[1]]), ...))
-}
-		
-		
+
+
 braincousens_logistic <- function (
-		fixed = c(NA, NA, NA, NA, NA), 
-		names = c("b", "c", "d", "e", "f"), 
-		method = c("1", "2", "3", "4"), 
-		ssfct = NULL, fctName, fctText) 
-{
+		fixed = c(NA, NA, NA, NA, NA),
+		names = c("b", "c", "d", "e", "f"),
+		method = c("1", "2", "3", "4"),
+		ssfct = NULL,
+		fctName,
+		fctText) {
 	numParm <- 5
 	if (!is.character(names) | !(length(names) == numParm)) {
 		stop("Not correct 'names' argument")
@@ -44,9 +45,7 @@ braincousens_logistic <- function (
 	fct <- function(dose, parm) {
 		parmMat <- matrix(parmVec, nrow(parm), numParm, byrow = TRUE)
 		parmMat[, notFixed] <- parm
-		parmMat[, 2] + (parmMat[, 3] + parmMat[, 5] * dose - 
-											parmMat[, 2])/(1 + exp(parmMat[, 1] * (dose - 
-																														 	parmMat[, 4])))
+		parmMat[, 2] + (parmMat[, 3] + parmMat[, 5] * dose - parmMat[, 2]) / (1 + exp(parmMat[, 1] * (dose -parmMat[, 4])))
 	}
 	if (FALSE) {
 		ssfct <- function(dataFra) {
@@ -65,10 +64,10 @@ braincousens_logistic <- function (
 			}
 			dose3 <- dose2[indexT2]
 			resp3 <- resp3[indexT2]
-			logitTrans <- log((startVal[3] - resp3)/(resp3 - 
-																							 	startVal[2] + 0.001))
+			logitTrans <- log((startVal[3] - resp3) / (resp3 -
+																								 	startVal[2] + 0.001))
 			logitFit <- lm(logitTrans ~ log(dose3))
-			startVal[4] <- exp((-coef(logitFit)[1]/coef(logitFit)[2]))
+			startVal[4] <- exp((-coef(logitFit)[1] / coef(logitFit)[2]))
 			startVal[1] <- coef(logitFit)[2]
 			return(startVal[notFixed])
 		}
@@ -90,40 +89,55 @@ braincousens_logistic <- function (
 		t1 <- parmMat[, 3] - parmMat[, 2] + parmMat[, 5] * dose
 		t2 <- exp(parmMat[, 1] * (dose - parmMat[, 4]))
 		t3 <- 1 + t2
-		t4 <- (1 + t2)^(-2)
-		cbind(-t1 * t2 * t4, 1 - 1/t3, 1/t3, t1 * t2 * parmMat[, 1] * t4, dose/t3)[, notFixed]
+		t4 <- (1 + t2) ^ (-2)
+		cbind(-t1 * t2 * t4, 1 - 1 / t3, 1 / t3, t1 * t2 * parmMat[, 1] * t4, dose /
+						t3)[, notFixed]
 	}
 	deriv2 <- NULL
-	edfct <- function(parm, respl, reference, type, lower = 0.001, 
-										upper = 1000, ...) {
+	edfct <- function(
+		parm,
+		respl,
+		reference,
+		type,
+		lower = 0.001,
+		upper = 1000,
+		...) {
 		interval <- c(lower, upper)
 		parmVec[notFixed] <- parm
 		p <- EDhelper(parmVec, respl, reference, type)
-		tempVal <- (100 - p)/100
+		tempVal <- (100 - p) / 100
 		helpEqn <- function(dose) {
 			expVal <- exp(parmVec[1] * (log(dose) - log(parmVec[4])))
-			parmVec[5] * (1 + expVal * (1 - parmVec[1])) - (parmVec[3] - 
-																												parmVec[2]) * expVal * parmVec[1]/dose
+			parmVec[5] * (1 + expVal * (1 - parmVec[1])) - 
+				(parmVec[3] - parmVec[2]) * expVal * parmVec[1] / dose
 		}
 		maxAt <- uniroot(helpEqn, interval)$root
 		eqn <- function(dose) {
-			tempVal * (1 + exp(parmVec[1] * (log(dose) - log(parmVec[4])))) - 
-				(1 + parmVec[5] * dose/(parmVec[3] - parmVec[2]))
+			tempVal * (1 + exp(parmVec[1] * (log(dose) - log(parmVec[4])))) -
+				(1 + parmVec[5] * dose / (parmVec[3] - parmVec[2]))
 		}
 		EDp <- uniroot(eqn, lower = maxAt, upper = upper)$root
 		EDdose <- EDp
 		tempVal1 <- exp(parmVec[1] * (log(EDdose) - log(parmVec[4])))
 		tempVal2 <- parmVec[3] - parmVec[2]
-		derParm <- c(tempVal * tempVal1 * (log(EDdose) - log(parmVec[4])), 
-								 -parmVec[5] * EDdose/((tempVal2)^2), parmVec[5] * 
-								 	EDdose/((tempVal2)^2), -tempVal * tempVal1 * 
-								 	parmVec[1]/parmVec[4], -EDdose/tempVal2)
-		derDose <- tempVal * tempVal1 * parmVec[1]/EDdose - 
-			parmVec[5]/tempVal2
-		EDder <- derParm/derDose
+		derParm <-
+			c(
+				tempVal * tempVal1 * (log(EDdose) - log(parmVec[4])),
+				-parmVec[5] * EDdose / ((tempVal2) ^ 2),
+				parmVec[5] *
+					EDdose / ((tempVal2) ^ 2),
+				-tempVal * tempVal1 *
+					parmVec[1] / parmVec[4],
+				-EDdose / tempVal2
+			)
+		derDose <- tempVal * tempVal1 * parmVec[1] / EDdose -
+			parmVec[5] / tempVal2
+		EDder <- derParm / derDose
 		return(list(EDp, EDder[notFixed]))
 	}
-	maxfct <- function(parm, lower = 0.001, upper = 1000) {
+	maxfct <- function(parm,
+										 lower = 0.001,
+										 upper = 1000) {
 		parmVec[notFixed] <- parm
 		if (parmVec[1] < 1) {
 			stop("Brain-Cousens model with b<1 not meaningful")
@@ -134,22 +148,32 @@ braincousens_logistic <- function (
 		optfct <- function(t) {
 			expTerm1 <- parmVec[5] * t
 			expTerm2 <- exp(parmVec[1] * (log(t) - log(parmVec[4])))
-			return(parmVec[5] * (1 + expTerm2) - (parmVec[3] - 
-																							parmVec[2] + expTerm1) * expTerm2 * parmVec[1]/t)
+			return(parmVec[5] * (1 + expTerm2) - (parmVec[3] -
+																							parmVec[2] + expTerm1) * expTerm2 * parmVec[1] /
+						 	t)
 		}
 		ED1 <- edfct(parm, 1, lower, upper)[[1]]
 		doseVec <- exp(seq(log(1e-06), log(ED1), length = 100))
-		maxDose <- uniroot(optfct, c((doseVec[optfct(doseVec) > 
+		maxDose <- uniroot(optfct, c((doseVec[optfct(doseVec) >
 																						0])[1], ED1))$root
-		return(c(maxDose, fct(maxDose, matrix(parm, 1, length(names)))))
+		return(c(maxDose, fct(maxDose, matrix(
+			parm, 1, length(names)
+		))))
 	}
-	returnList <- list(fct = fct, ssfct = ssfct, names = names, 
-										 deriv1 = deriv1, deriv2 = deriv2, edfct = edfct, maxfct = maxfct, 
-										 name = ifelse(missing(fctName), as.character(match.call()[[1]]), 
-										 							fctName), text = ifelse(missing(fctText), "Brain-Cousens (hormesis)", 
-										 																			fctText), noParm = sum(is.na(fixed)))
+	returnList <- list(
+		fct = fct,
+		ssfct = ssfct,
+		names = names,
+		deriv1 = deriv1,
+		deriv2 = deriv2,
+		edfct = edfct,
+		maxfct = maxfct,
+		name = ifelse(missing(fctName), as.character(match.call()[[1]]),
+									fctName),
+		text = ifelse(missing(fctText), "Brain-Cousens (hormesis)",
+									fctText),
+		noParm = sum(is.na(fixed))
+	)
 	class(returnList) <- "braincousens_logistic"
 	invisible(returnList)
 }
-
-################################################################################
--- multiple_fit_testing.R
+++ multiple_fit_testing.R
@@ -1,32 +1,12 @@
+# Load necessary packages
 require(conflicted)
 require(pacman)
 source("drc_logistic_functions.R")
 
+# Load data
 aba_key <- fread("aba_key.tsv")
-
-p_load(
-	"data.table",
-	"tidyverse",
-	"broom",
-	"modelr")
-
-p_load_current_gh(
-	"DoseResponse/drcData",
-	"ryandward/drc",
-	"hrbrmstr/hrbrthemes")
-
-conflicted::conflicts_prefer(
-	gtools::permute,
-	dplyr::filter,
-	dplyr::select,
-	drc::gaussian)
-
-# https://jgeb.springeropen.com/articles/10.1186/s43141-020-00048-4
-
-# all genes
 interested.genes <- fread("curated_names.tsv") %>% pull(unique_name)
-
-interested.conditions <- c(
+interested.conditions <- c( # List of conditions
 	"None_0_T1 - None_0_T0",
 	"None_0_T2 - None_0_T0",
 	"Colistin_0.44_T1 - None_0_T0",
@@ -40,30 +20,25 @@ interested.conditions <- c(
 	"Imipenem_0.06_T1 - None_0_T0",
 	"Imipenem_0.09_T1 - None_0_T0",
 	"Imipenem_0.06_T2 - None_0_T0",
-	"Imipenem_0.09_T2 - None_0_T0")
+	"Imipenem_0.09_T2 - None_0_T0") 
 
-# read results
-
+# Read results
 melted_results <- fread("Results/melted_results.tsv.gz", sep = "\t")
-
 median_melted_results <- fread("Results/median_melted_results.tsv.gz", sep = "\t")
-
 curated_names <- fread("curated_names.tsv", sep = "\t")
-
 aba_key <- fread("aba_key.tsv", sep = "\t")
 
-
+# Pre-process data
 melted_results$y_pred <- NULL
-
 melted_results[aba_key, on = .(spacer), y_pred := y_pred]
 
+# Define error handling functions
 drm.try <- possibly(drm, otherwise = NA)
 augment.try <- possibly(augment, otherwise = NA)
 glance.try <- possibly(glance, otherwise = NA)
 tidy.try <- possibly(tidy, otherwise = NA)
 
-#estimate dose-response models
-
+# Estimate dose-response models
 mismatches <- melted_results %>%
 	filter(y_pred > 0) %>%
 	# mutate(y_pred = case_when(y_pred < 0 ~ 0, TRUE ~ y_pred)) %>%
@@ -81,44 +56,13 @@ mismatches <- melted_results %>%
 			rename(response.max = LFC.adj)) %>%
 	nest(data = c(-condition, -unique_name, -response.max))
 
-################################################################################
-
+# Test genes and parameters
 test.genes <- c("murA", "rpmB", "aroC", "GO593_00515", "glnS", "nuoB", "lpxC")
-
-LL.4.parameters <- c("shape", "min_value", "max_value", "kd_50")
+L.4.parameters <- c("shape", "min_value", "max_value", "kd_50")
 BC.5.parameters <- c("shape", "min_value", "max_value", "kd_50", "hormesis")
 
-################################################################################
-
-
-
-L.4.model <- mismatches %>%
-	filter(unique_name %in% test.genes) %>%
-	mutate(fit = case_when(
-		response.max > 0 ~ map2(
-			data,
-			response.max,
-			~drm.try(
-				data = .x, 
-				LFC.adj ~ y_pred, 
-				control = drmc(method = "Nelder-Mead", noMessage = TRUE, maxIt = 50000), 
-				fct = L.4(fixed = c(NA, 0, .y, NA), names = LL.4.parameters)
-			)
-		),
-		response.max < 0 ~ map2(
-			data,
-			response.max,
-			~drm.try(
-				data = .x, 
-				LFC.adj ~ y_pred, 
-				control = drmc(method = "Nelder-Mead", noMessage = TRUE, maxIt = 50000), 
-				fct = L.4(fixed = c(NA, .y, 0, NA), names = LL.4.parameters)
-			)
-		)
-	))
-
 BC.5.model <- mismatches %>%
-	filter(unique_name %in% test.genes) %>%
+	# filter(unique_name %in% test.genes) %>%
 	mutate(fit = case_when(
 		response.max > 0 ~ map2(
 			data,
@@ -142,17 +86,38 @@ BC.5.model <- mismatches %>%
 		)
 	))
 
-################################################################################
+# Define BC.5.model and BC.5.reduced
+BC.5.reduced <- mismatches %>%
+	# filter(unique_name %in% test.genes) %>%
+	mutate(fit = case_when(
+		response.max > 0 ~ map2(
+			data,
+			response.max,
+			~drm.try(
+				data = .x, 
+				LFC.adj ~ y_pred, 
+				control = drmc(method = "Nelder-Mead", noMessage = TRUE, maxIt = 50000), 
+				fct = BC.5.logistic(fixed = c(NA, 0, .y, NA, 0), names = BC.5.parameters))),
+		response.max < 0 ~ map2(
+			data,
+			response.max,
+			~drm.try(
+				data = .x, 
+				LFC.adj ~ y_pred, 
+				control = drmc(method = "Nelder-Mead", noMessage = TRUE, maxIt = 50000), 
+				fct = BC.5.logistic(fixed = c(NA, .y, 0, NA, 0), names = BC.5.parameters)))))
 
+# Define LRT and ANOVA calculation functions
 calculate_lrt <- function(this.gene, this.condition, this.HA, this.H0) {
-	# Filter the BC.5.model and LL.4.model data frames for the given gene and condition
+	
+	# Filter the BC.5.model and LBC.5.reduced data frames for the given gene and condition
 	this.HA <- BC.5.model %>%
 		filter(unique_name == this.gene) %>%
 		filter(condition == this.condition) %>%
 		select(fit) %>%
 		pull(fit)
 	
-	this.H0 <- LL.4.model %>%
+	this.H0 <- BC.5.reduced %>%
 		filter(unique_name == this.gene) %>%
 		filter(condition == this.condition) %>%
 		select(fit) %>%
@@ -160,22 +125,22 @@ calculate_lrt <- function(this.gene, this.condition, this.HA, this.H0) {
 	
 	# Perform the likelihood ratio test
 	lrt.result <- lrtest(this.HA[[1]], this.H0[[1]])
-
+	
 	# Return the likelihood ratio test result
 	# return(lrt.result)
 	return(lrt.result)
 }
 
-
 calculate_anova <- function(this.gene, this.condition, this.HA, this.H0) {
-	# Filter the BC.5.model and LL.4.model data frames for the given gene and condition
+	
+	# Filter the BC.5.model and LBC.5.reduced data frames for the given gene and condition
 	this.HA <- BC.5.model %>%
 		filter(unique_name == this.gene) %>%
 		filter(condition == this.condition) %>%
 		select(fit) %>%
 		pull(fit)
 	
-	this.H0 <- LL.4.model %>%
+	this.H0 <- BC.5.reduced %>%
 		filter(unique_name == this.gene) %>%
 		filter(condition == this.condition) %>%
 		select(fit) %>%
@@ -185,9 +150,6 @@ calculate_anova <- function(this.gene, this.condition, this.HA, this.H0) {
 	anova.result <- anova(this.HA[[1]], this.H0[[1]])
 	
 	# Return the likelihood ratio test result
-	# return(lrt.result)
+	# return(lrt.result) 
 	return(anova.result)
 }
-
-
-############################
