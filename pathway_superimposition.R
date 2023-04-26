dd3a60c9c6476865630250a5f1904e9d4990949a
Ryan Valverde de Ward <ryandward@gmail.com>
2023-04-26 08:28:11 -0500

    add: rework pathway superimposition to use loess

--- pathway_superimposition.R
+++ pathway_superimposition.R
@@ -48,112 +48,24 @@ annotated_fit_predictions <-
 	mutate(Pathway = factor(Pathway, levels = c("Ribosome", "PG/Division", "Ox Phos", "tRNA Ligase", "LOS"))) %>%
 	filter(!is.na(Pathway))
 
-rough_fits <- annotated_fit_predictions %>% 
-	filter(Condition %in% c(
-		"None_0_T1 - None_0_T0")) %>%
-	mutate(binned_y_pred = round(y_pred, 6)) %>% 
-	group_by(Condition, Pathway, binned_y_pred) %>%
-	summarise(binned_fit = mean(.fitted)) 
+pathway_colors <- c(
+	"Ox Phos" = "#6A3D9A",
+	"LOS" = "#33A02C",
+	"tRNA Ligase" = "#1F78B4",
+	"PG/Division" = "#FF7F00",
+	"Ribosome" = "#E31A1C")
 
-rough_fits <- rough_fits %>%
-	nest(data = c(-Condition, -Pathway))
-
-
-
-rough_fits <- rough_fits %>% 
-	mutate(
-		fit = map(
-			data, 
-			~ drm.try(
-				data = .x, 
-				binned_fit ~ binned_y_pred, 
-				fct = BC.5(names = c("hill", "min_value", "max_value", "kd_50", "hormesis")))))
-
-rough_fits <- rough_fits %>% 
-	mutate(results = map(fit, glance)) %>%
-	mutate(p.vals = map(fit, tidy)) %>%
-	mutate(results = map(results, ~mutate(.x, logLik = c(logLik)))) %>%
-	unnest(results)
-
-rough_fits <- rough_fits %>%
-	mutate(
-		kd_50.tibble = map(
-			p.vals, 
-			~filter(.x, term == 'kd_50') %>%
-				select(p.value) %>%
-				rename (., vuln.p = p.value))) %>%
-	mutate(
-		vuln.tibble = map2(
-			fit, 
-			p.vals, 
-			~augment.try(
-				.x,
-				newdata = .y %>% filter (term == "kd_50") %>% select (estimate)) %>% 
-				rename (., vuln.est = .fitted, vuln.kd_50 = estimate))) %>%
-	mutate(
-		hill.tibble = map(
-			p.vals, 
-			~filter(.x, term == 'hill') %>%
-				select(estimate, p.value) %>%
-				rename (., hill.est = estimate, hill.p = p.value)))
-
-rough_fits <- rough_fits %>%
-	unnest(vuln.tibble) %>%
-	unnest(kd_50.tibble) %>%
-	unnest(hill.tibble) %>%
-	select(-c(p.vals))
-
-rough_fits <- rough_fits %>% 
-	mutate(predictions = map2(fit, data, ~augment.try(
-		.x,
-		newdata = expand.grid(
-			y_pred = seq(
-				min(.y$binned_y_pred), 
-				max(.y$binned_y_pred), 
-				length = 250)),
-		conf.int = T,
-		conf.level = 0.90)))
-
-rough_fit_predictions <- rough_fits %>% 
-	select(Pathway, Condition, predictions) %>% 
-	unnest(predictions)
-
-plot.graphic <- annotated_fit_predictions %>% 
-	filter(Condition %in% c(
-		"None_0_T1 - None_0_T0")) %>%
-	filter(Pathway != "Other") %>%
-	filter(!is.na(Pathway)) %>%
-	ggplot() +
-	geom_hline(
-		yintercept = 0, 
-		linetype = "dashed", 
-		color = "black", 
-		size = 0.5) +
-	geom_line(
-		size = 1.5, 
-		aes(
-			x = y_pred, 
-			y = .fitted, 
-			group = Gene,
-			colour = Pathway),
-		alpha = 0.5) +
-	geom_line(
-		data = rough_fit_predictions,
-		aes(x = y_pred, y = .fitted),
-		colour = "black",
-		lwd = 3
-	) +
-	xlab("Predicted Knockdown") +
-	ylab("Log-2 Fitness Foldchange") +
+annotated_fit_predictions %>%
+	filter(Condition %in% c("None_0_T1 - None_0_T0", "None_0_T2 - None_0_T0")) %>%
+	filter(Pathway %in% c("Ox Phos", "LOS")) %>%
+	ggplot(aes(x = y_pred, y = .fitted, group = AB19606, color = Pathway)) +
+	# Plot individual fitted lines for each gene (locus tag) within a pathway
+	geom_line(linewidth = 1.0, alpha = 0.25) +
+	# Calculate and plot the smoothed central tendency line for each pathway on the fly
+	stat_smooth(aes(group = Pathway), method = "loess", linetype = "dotdash", linewidth = 1.5, se = FALSE) +
+	# Apply the specified colors to the pathways
+	scale_color_manual(values = pathway_colors) +
+	labs(x = "y_pred", y = "Fitted values") +
 	doc_theme +
-	theme(legend.position = "none") +
-	facet_wrap(facets = c("Pathway"), nrow = 2) +
-	scale_colour_manual(
-		values = c(
-			"Ribosome" = "#FB9A99", 
-			"Ox Phos" = "#CAB2D6",
-			"LOS" = "#B2DF8A",
-			"PG/Division" = "#FDBF6F"))
+	facet_grid(Condition ~ Pathway, scales = "free_y")
 
-
-print(plot.graphic)
