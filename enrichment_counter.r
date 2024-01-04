88da4feb0ccc662a9bd772af0ac282077b7a6875
Ryan Ward <ryandward@gmail.com>
2024-01-04 07:44:25 -0600

    optimizations for enriching

--- enrichment_counter.r
+++ enrichment_counter.r
@@ -122,13 +122,22 @@ voom_fit <- voomLmFit(dge, aba_permut)
 
 
 contrasts <- makeContrasts(
-  T1 = T1_None - T0_None,
-  T2 = T2_None - T0_None,
+  # T1 = T1_None - T0_None,
+  # T2 = T2_None - T0_None,
   T1_Rifampicin = T1_Rifampicin - T1_None,
+  T2_Rifampicin = T2_Rifampicin - T2_None,
   T1_Colistin = T1_Colistin - T1_None,
-  T1_Col_Rif = T1_Colistin - T1_Rifampicin,
+  T2_Colistin = T2_Colistin - T2_None,
   T1_Imipenem = T1_Imipenem - T1_None,
-  T1_Rif_Imi = T1_Rifampicin - T1_Imipenem,
+  T2_Imipenem = T2_Imipenem - T2_None,
+  T1_Meropenem = T1_Meropenem - T1_None,
+  T2_Meropenem = T2_Meropenem - T2_None,
+  # T1_Rif_Imi = T1_Rifampicin - T1_Imipenem,
+  # T1_Rif_Mero = T1_Rifampicin - T1_Meropenem,
+  # T2_Rif_Imi = T2_Rifampicin - T2_Imipenem,
+  # T2_Rif_Mero = T2_Rifampicin - T2_Meropenem,
+  T1_Col_Rif = T1_Colistin - T1_Rifampicin,
+  T2_Col_Rif = T2_Colistin - T2_Rifampicin,
   levels = aba_permut
 )
 ##########################################################################################
@@ -232,7 +241,7 @@ locus_tags_list <- split(target_spacers_for_terms$spacer, target_spacers_for_ter
 gene_indices <- lapply(locus_tags_list, function(locus_tags) which(rownames(dge) %in% locus_tags))
 
 
-v <- voomWithQualityWeights(dge, aba_permut, plot = TRUE)
+v <- voom(dge, aba_permut, plot = TRUE)
 
 
 v_targets <- v$E %>%
@@ -257,7 +266,7 @@ v_targets[spacer == target, weight := max(v_targets$y_pred, na.rm = TRUE)]
 
 v_targets[mismatches >=1, weight := y_pred]
 
-v_targets$weight <- rescale(as.numeric(v_targets$weight), to = c(1, 1000))
+v_targets$weight <- rescale(as.numeric(v_targets$weight), to = c(1, 100))
 
 # Perform the competitive gene set test for all gene sets
 all_sets <- lapply(colnames(contrasts), function(contrast_name) {
@@ -278,53 +287,34 @@ all_sets <- lapply(colnames(contrasts), function(contrast_name) {
   #######################
 
 
-all_sets <- all_sets %>% 
-#   inner_join(enrichments) %>% 
-#   inner_join(v_targets) %>%
-#   inner_join(term_stats) %>% 
-#   group_by(contrast, term, description) %>% 
-#   nest(locus_tags = locus_tag) %>% 
-#   group_by(locus_tags, contrast) %>% 
-#   mutate(missing_genes = gene_count - genes_targeted) %>%
-#   arrange(FDR, missing_genes) %>% 
-#   slice(1) %>% 
-#   ungroup %>% 
+best_sets <- all_sets %>% 
+  inner_join(enrichments) %>% 
+  inner_join(v_targets) %>%
+  inner_join(term_stats) %>% 
+  group_by(contrast, term, description) %>% 
+  nest(locus_tags = locus_tag) %>% 
+  group_by(locus_tags, contrast) %>% 
+  mutate(missing_genes = gene_count - genes_targeted) %>%
+  arrange(FDR, missing_genes) %>% 
+  slice(1) %>% 
+  ungroup %>% 
   rename(guide_count = NGenes) %>%
-#   select(term, guide_count, Direction, PValue, FDR, contrast, description, genes_targeted, gene_count)  %>% unique()  %>% 
+  select(term, guide_count, Direction, PValue, FDR, contrast, description, genes_targeted, gene_count)  %>% unique()  %>% 
   data.table()
 
 create_plot <- function(full_data, targets, enrichments, sets) {
   set_name <- deparse(substitute(sets))
 
   plot_data <- full_data %>%
-      inner_join(aba_design) %>%
-      filter(induced == TRUE) %>%
-      # filter(
-      #   drug_dose %like% "None"| 
-      #   drug_dose %like% "Imipenem" |
-      #   drug_dose %like% "Colistin_0.44" | 
-      #   drug_dose %like% "Rifampicin_0.34") %>%
-
+    inner_join(aba_design) %>%
+    filter(induced == TRUE) %>%
     rename(sample = condition) %>%
-    # select(-type) %>%
-    # mutate(imipenem = ifelse(is.na(imipenem), "Stock", imipenem), induced = ifelse(is.na(induced), "Stock", induced)) %>%
     group_by(sample) %>%
-    # mutate(imipenem = factor(imipenem, levels = c("Stock", "0", "0.125", "0.25"))) %>%
-    # mutate(induced = factor(induced, levels = c("Stock", "FALSE", "TRUE"))) %>%
     mutate(cpm = cpm(count)) %>%
-    # filter(type %like% "perfect") %>%
     ungroup() %>%
     inner_join(targets, by = "spacer", relationship = "many-to-many") %>%
     inner_join(enrichments, relationship = "many-to-many") %>%
-    inner_join(
-      rbind(
-        # sets %>% filter(Direction == "Down") %>% arrange(FDR) %>% head(6),
-        # sets %>% filter(Direction == "Up") %>% arrange(FDR) %>% head(6)
-        sets %>%
-          arrange(FDR) %>%
-          head(12)
-      )
-    ) %>%
+    inner_join(sets %>% arrange(FDR) %>% head(12)) %>%
     filter(FDR <= 0.05) %>%
     group_by(factor(drug_dose), factor(induced), term) %>%
     ungroup() %>%
@@ -334,13 +324,13 @@ create_plot <- function(full_data, targets, enrichments, sets) {
     mutate(facet_title = paste0("**", term, "**", " — ", Direction, "<br>", description)) %>%
     # mutate(facet_title = sub("([^ \n]+)", "**\\1**", facet_title)) %>%
     mutate(facet_title = gsub("\n", "<br>", facet_title)) %>%
-    mutate(facet_title = paste(facet_title, paste0("**FDR** = ", signif(FDR, 2), ", *n* = ", guide_count), sep = "<br>")) %>%
+    mutate(facet_title = paste(facet_title, paste0("**FDR** = ", signif(FDR, 2), ", *n* = ", guide_count), contrast, sep = "<br>")) %>%
     mutate(facet_title = factor(facet_title, levels = facet_title %>% unique()))
 
   ggplot(plot_data, aes(y = cpm, x = factor(timing), group = interaction(factor(drug_dose), factor(timing)))) +
     geom_tile(data = data.frame(timing= "T2"), aes(x = timing, y = 0), width = 1, height = Inf, fill = "grey50", alpha = 0.2, inherit.aes = FALSE) +
-    geom_sina(aes(weight = as.numeric(weight), color = factor(drug_dose), size = weight), alpha = 0.5, shape = 16) +
-    geom_violin(aes(weight = as.numeric(weight)), alpha = 0.25, draw_quantiles = c(0.25, 0.5, 0.75), position = "dodge") +
+    geom_sina(aes(weight = as.numeric(weight), color = factor(drug_dose), size = weight), alpha = 0.5, shape = 16, position = "dodge", scale = "width") +
+    geom_violin(aes(weight = as.numeric(weight)), alpha = 0.25, draw_quantiles = c(0.25, 0.5, 0.75), position = "dodge", scale = "width") +
     facet_wrap(~facet_title, nrow = 3, scales = "free_y") +
     scale_size(range = c(0.25, 2.5)) +
     # use some nice colors for fill and color
@@ -364,7 +354,7 @@ create_plot <- function(full_data, targets, enrichments, sets) {
     )
 }
 
-create_plot(aba, v_targets, enrichments, all_sets[contrast == "T1_Col_Rif", ])
+create_plot(aba, v_targets, enrichments, best_sets[contrast == "T2_Col_Rif"])
 
 
 
@@ -379,12 +369,15 @@ plot_results <- function(results, targets, enrichments, sets) {
     arrange(FDR) %>%
     pull(term) %>%
     unique() %>%
-    head(5)
+    head(6)
 
   # Filter the rows that have those terms
   filtered_sets <- sets %>%
     filter(term %in% lowest_FDR_terms) %>%
-    arrange(FDR) %>% rename(winning_contrast = contrast) %>% unique()
+    arrange(FDR) %>% rename(winning_contrast = contrast) %>% unique() %>%
+    group_by(term) %>%
+    arrange(FDR) %>% 
+    slice(1)
 
   plot_data <- results %>%
     filter(!contrast %in% c("T1", "T2")) %>%
@@ -405,8 +398,8 @@ plot_results <- function(results, targets, enrichments, sets) {
 
   ggplot(plot_data, aes(y = logFC, x = factor(contrast))) +
     # geom_tile(data = data.frame(timing= "T2"), aes(x = timing, y = 0), width = 1, height = Inf, fill = "grey50", alpha = 0.2, inherit.aes = FALSE) +
-    geom_sina(aes(weight = as.numeric(-log10(adj.P.Val)), color = factor(contrast), size = -log10(adj.P.Val)), alpha = 0.5, shape = 16) +
-    geom_violin(aes(weight = as.numeric(-log10(adj.P.Val))), alpha = 0.35, draw_quantiles = c(0.25, 0.5, 0.75), position = "dodge") +
+    geom_sina(aes(weight = as.numeric(-log10(adj.P.Val)), color = factor(contrast), size = -log10(adj.P.Val)), alpha = 0.5, shape = 16, scale = "width") +
+    geom_violin(aes(weight = as.numeric(-log10(adj.P.Val))), alpha = 0.35, draw_quantiles = c(0.25, 0.5, 0.75), position = "dodge", scale = "width") +
     facet_wrap(~facet_title, nrow = 3, scales = "free_y") +
     scale_size(range = c(0.25, 2.5)) +
     ggtitle(set_name) +
@@ -421,12 +414,12 @@ plot_results <- function(results, targets, enrichments, sets) {
     theme_minimal() +
     theme(
       strip.text = element_markdown(),
-      axis.text.x = element_text(size = rel(1.3), color = "black"),
+      axis.text.x = element_text(size = rel(1.3), color = "black", angle = 45, hjust = 1),
       axis.title.y = element_text(size = rel(1.3), color = "black")
     )
 }
 
-plot_results(results, v_targets, enrichments, all_sets)
+plot_results(results, v_targets, enrichments, best_sets[contrast == "T2_Col_Rif"])
 
 
 
