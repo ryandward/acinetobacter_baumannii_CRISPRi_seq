5c5f112badb1aec3ca93b3b4a577c81384b7df2b
Ryan Ward <ryandward@gmail.com>
2024-04-01 16:32:53 -0500

    Refactor scoring function and update enrichment plotter

--- Chemical-Genomics/enrichment_application_plotpad.r
+++ Chemical-Genomics/enrichment_application_plotpad.r
@@ -5,9 +5,22 @@ library(dplyr)
 library(ggplot2)
 library(scales)
 
-score_set <- function(selected_contrast_pattern) {
+score_set <- function(selected_contrast_pattern, pattern = TRUE) {
+
+  if(pattern) {
+    these_contrasts <- all_sets |>
+      filter(contrast %like% selected_contrast_pattern) |>
+      pull(contrast)
+  }
+
+  else(
+    these_contrasts <- all_sets |>
+      filter(contrast %in% selected_contrast_pattern) |>
+      pull(contrast)
+  )
+
   all_sets |>
-    filter(contrast %like% selected_contrast_pattern) |>
+    filter(contrast %like% selected_contrast_pattern) |> # could relace with c("contrast1", "contrast2")
     select(Direction, term, description, FDR, genes_targeted, gene_count, contrast) |>
     mutate(FDR = signif(FDR, 2)) |>
     mutate(prop_targ = signif(genes_targeted / gene_count, 2)) |>
@@ -22,22 +35,28 @@ score_set("*") |>
   dcast(term + description + genes_targeted + gene_count ~ contrast, value.var = "score")
 # then write to clipboard and paste into excel with clipr::write_clip()
 
-create_enrichment_plot <- function(selected_term, selected_contrast_pattern) {
+create_enrichment_plot <- function(selected_term, selected_contrast_patterns) {
   # Filter 'term_stats' using the 'selected_term' argument
   title <- term_stats %>%
     filter(term == selected_term) %>%
     pull(description)
   title <- paste(title, " (", selected_term, ")", sep = "")
-
-  # Construct the plot with dynamic filtering based on 'selected_term' and 'selected_contrast_pattern'
+  
+  # Helper function to check if any of the patterns match the contrast
+  matches_any_pattern <- function(contrast, patterns) {
+    any(sapply(patterns, function(pattern) grepl(pattern, contrast)))
+  }
+  
+  # Construct the plot with dynamic filtering based on 'selected_term' and 'selected_contrast_patterns'
   enrichment_plot <- contrast_assignments %>%
     inner_join(group_assignments) %>%
     inner_join(
       all_sets %>%
-        filter(term == selected_term & grepl(selected_contrast_pattern, contrast)) %>%
-        inner_join(contrast_assignments) %>%
-        mutate(contrast = factor(contrast, levels = unique(contrast)))
+        filter(term == selected_term) %>%
+        filter(sapply(contrast, matches_any_pattern, selected_contrast_patterns))
     ) %>%
+        inner_join(contrast_assignments) %>%
+        mutate(contrast = factor(contrast, levels = unique(contrast))) %>%
     mutate(
       label = paste(
         contrast,
@@ -59,7 +78,7 @@ create_enrichment_plot <- function(selected_term, selected_contrast_pattern) {
     mutate(group = factor(group, levels = unique(group))) %>%
     ggplot(aes(x = as.character(assignment), y = cpm)) +
     geom_tile(aes(alpha = factor(ifelse(FDR <= 0.05, "highlight", "no_highlight"))), width = Inf, height = Inf, fill = "light grey") +
-    geom_sina(aes(weight = weight), shape = 21, color = "darkgrey", lwd = 0.1) +
+    geom_sina(aes(weight = weight, size = y_pred), shape = 21, color = "darkgrey", lwd = 0.1) +
     geom_violin(aes(weight = weight), alpha = 0.0, draw_quantiles = c(0.25, 0.5, 0.75), lwd = 0.75) +
     scale_alpha_manual(values = c("highlight" = 0.00, "no_highlight" = 0.025), guide = FALSE) +
     scale_size(range = c(0.1, 3)) +
@@ -68,15 +87,34 @@ create_enrichment_plot <- function(selected_term, selected_contrast_pattern) {
     labs(x = NULL, y = "Counts per Million") +
     ggtitle(title) +
     scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0) +
-    scale_size_continuous(range = c(5, 1), limits = c(0, 1)) +
+    scale_size_continuous(range = c(1, 5), limits = c(0, 1)) +
     theme_minimal()
 
   print(enrichment_plot)
 }
 
-create_enrichment_plot("CL:1517", "cycline")
+# variety of things for the cyclines
+create_enrichment_plot("CL:1517", "cycline") # heme copper terminal oxidase
 create_enrichment_plot("CL:1490", "cycline")
 create_enrichment_plot("GO:0015920", "cycline")
 create_enrichment_plot("GO:0045263", "cycline")
-create_enrichment_plot("GO:0140101", "polymyxin")
-create_enrichment_plot("CL:912", "cycline")
+create_enrichment_plot("GO:0008137", "cycline")
+create_enrichment_plot("CL:1490", "cycline")
+
+create_enrichment_plot("GO:0140101", c("colistin", "polymyxin"))
+
+
+create_enrichment_plot("GO:0071554", "penem") #direct target
+create_enrichment_plot("CL:2527", "chlorhexidine" ) #direct target. minor differences in efficacy
+create_enrichment_plot("GO:0003887", "floxacin" ) #direct target, different mechanism of action, different class of drug
+
+create_enrichment_plot("CL:332", c("carvacrol", "thymol")) #NADH
+create_enrichment_plot("GO:0042886", c("carvacrol", "thymol")) #potential target
+
+create_enrichment_plot("CL:1490", c("fosfo", "indole"))
+create_enrichment_plot("GO:0005216", "trimethoprim")
+
+create_enrichment_plot("CL:535", "DMSO")
+
+create_enrichment_plot("CL:2556", c("EDTA", "TPEN"))
+create_enrichment_plot("CL:1265", c("EDTA", "TPEN"))
